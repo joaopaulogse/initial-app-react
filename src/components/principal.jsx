ba39b668285317eb941dd4c39240a015596f45c5
Joao Paulo Gonçalves <joaopaulogse@gmail.com>
2017-10-28 16:00:14 -0200

    interação com api do google maps

--- src/components/principal.jsx
+++ src/components/principal.jsx
@@ -1,40 +1,48 @@
 import React, {Component} from 'react'
+import {  withScriptjs,GoogleMap, Marker, withGoogleMap } from "react-google-maps";
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import {  Field, reduxForm } from 'redux-form'
 import PropTypes from 'prop-types'
 
-import {changeInput, changeFirebase} from './principalActions'
+import { changeInput, changeFirebase } from './principalActions'
 
 import { database } from '../services/firebase'
 class Principal extends Component{ 
     
     constructor(props){
         super(props)
-    }
-    componentDidMount(){
-        var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-        var mapOptions = {
-        zoom: 4,
-        center: myLatlng
+        this.state ={
+            markers:[{
+                position:{
+                    lat:-16.0138654,
+                    lng:-48.0614247
+                }
+            }]
         }
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            title:"Hello World!"
-        });
-
-        // To add the marker to the map, call setMap();
-        marker.setMap(map);
-                
+        navigator.geolocation.getCurrentPosition(success=>{
+            this.setState({   
+                    markers:[...this.state, ...{position:{lat:success.coords.latitude, lng:success.coords.longitude}}]
+                })
+        })
+        // this.setState({   
+        //         markers:[...this.state, ...{position:{lat:-16.013872,lng:-48.061423}} ]
+        //     })
     }
-    
+        
     render(){
         const { nome, changeInput, email, form } = this.props
         return (
         <div className='container'>
-            <div id="map"></div>
+            <Map isMarkerShown
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCLZ_Y-mXEK51keuawkneoXzmilUabHjQc"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+                markers={this.state.markers}>
+               
+            </Map>
+            
         </div>
         )
     }
@@ -49,5 +57,15 @@ Principal.propTypes = {
     nome:PropTypes.string
 }
 
+const Map = withScriptjs(withGoogleMap((props) =>
+    <GoogleMap
+    defaultZoom={8}
+    defaultCenter={props.markers[0].position}
+    >
+    {props.isMarkerShown && props.markers.map((marker, index)=>(
+        <Marker key={index} position={marker.position}/>
+    ))}
+    </GoogleMap>
+))
 
 export default connect(mapStateToProps, mapDispachToProps)(Principal)
