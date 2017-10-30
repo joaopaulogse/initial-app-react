c3fe50409175e83b12aa38b1059cf3c5d5e7c43e
Joao Paulo <joaopaulogse@gmail.com>
2017-10-30 08:52:53 -0200

    merge

--- src/components/principal.jsx
+++ src/components/principal.jsx
@@ -12,29 +12,15 @@ class Principal extends Component{
     constructor(props){
         super(props)
     }
-    componentDidMount(){
-        var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
-        var mapOptions = {
-        zoom: 4,
-        center: myLatlng
-        }
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            title:"Hello World!"
-        });
-
-        // To add the marker to the map, call setMap();
-        marker.setMap(map);
-                
-    }
-    
     render(){
         const { nome, changeInput, email, form } = this.props
         return (
         <div className='container'>
-            <div id="map"></div>
+            <input id='nome' type="text" name='nome' onChange={changeInput} value={nome}/> <br/>
+            <input type="text" name='email' onChange={changeInput} value={email}/>
+            
+            <h2>Nome: {nome}</h2><br/>
+            <h2>Email: {email}</h2>
         </div>
         )
     }
